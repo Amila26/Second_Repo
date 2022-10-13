b41b061c126bdf623c0f99aef3e0d7e6955744b0
Amila26 <amilaindika26@gmail.com>
2022-10-13 10:45:19 +0100

    Change the name as Kasun

--- Print.py
+++ Print.py
@@ -2,7 +2,7 @@ print("Helow")
 for i in range(1,10):
   print(i ,end=" ")
 print("\n")
-for letter in "Amila":
+for letter in "Kasun":
   print(letter,end=" ")
 y=int(input("input integer Value"))
 print(y)
